c7b100efb9a4f18f191e48d5a79e584805d71110
Sebastian Albert <albert@math.uni-goettingen.de>
2016-12-28 23:44:18 +0100

    initial setup

---
+++ src/main/java/com/github/aptd/osm2railml/CMain.java
@@ -0,0 +1,13 @@
+package com.github.aptd.osm2railml;
+
+/**
+ * Hello world!
+ *
+ */
+public class CMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/github/aptd/osm2railml/CMainTest.java
@@ -0,0 +1,38 @@
+package com.github.aptd.osm2railml;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CMainTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CMainTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CMainTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testCMain()
+    {
+        assertTrue( true );
+    }
+}
