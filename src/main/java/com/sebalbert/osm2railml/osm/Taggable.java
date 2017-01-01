4d48f9b5745cc9273dc28dc46223a460990d1914
Sebastian Albert <albert@math.uni-goettingen.de>
2017-01-01 18:56:27 +0100

    set encoding to UTF-8, require Java 8

---
+++ src/main/java/com/sebalbert/osm2railml/osm/Taggable.java
@@ -0,0 +1,10 @@
+package com.sebalbert.osm2railml.osm;
+
+/**
+ * Base class for Node, Way and Relation, because all of them can have properties
+ */
+public class PropertiesContainer {
+
+    public Map<String, String> property;
+
+}
